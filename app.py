de4c680169bb9a221c0d9e790b7e1ca8d395e9ab
ssjss77 <43239391+ssjss77@users.noreply.github.com>
2020-09-06 00:04:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
     data_unseen = pd.DataFrame([final], columns = cols)
     prediction = predict_model(model, data=data_unseen, round = 0)
     prediction = int(prediction.Label[0])
-    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
+    return render_template( 'home.html', pred='La facture attendue est {} €'.format( prediction ) )
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
